b84d2f04c3070e94d4355ce6f2ed0ca3cfb560d0
Marco Severini <marcoseverini5@gmail.com>
2025-10-30 16:12:06 +0100

    fix

--- service/api/helpers.go
+++ service/api/helpers.go
@@ -8,11 +8,13 @@ import (
 // Funzione per inviare una risposta di errore JSON con il codice di stato e il messaggio specificati
 func (rt *_router) sendErrorResponse(w http.ResponseWriter, statusCode int, message string) {
 
-	w.WriteHeader(statusCode)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	
 	errRes := ErrorResponse{
 		Message: message,
 	}
+	
 	_ = json.NewEncoder(w).Encode(errRes)
 }
 
