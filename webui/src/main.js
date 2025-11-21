935171ca041d9f5a0322220c5d5401d5e0ee1dc5
Marco Severini <marcoseverini5@gmail.com>
2025-11-21 16:07:40 +0100

    bug fix frontend feedback 0

--- webui/src/main.js
+++ webui/src/main.js
@@ -4,7 +4,7 @@ import router from './router' // Importa il router configurato
 import ErrorMsg from './components/ErrorMsg.vue' // Importa il componente per i messaggi di errore
 import LoadingSpinner from './components/LoadingSpinner.vue' // Importa il componente per lo spinner di caricamento
 
-import 'bootstrap/dist/css/bootstrap.min.css' // Importa lo stile di Bootstrap
+// import 'bootstrap/dist/css/bootstrap.min.css' // Importa lo stile di Bootstrap
 import './assets/dashboard.css' // Importa gli stili personalizzati
 import './assets/main.css' // Importa altri stili personalizzati
 
