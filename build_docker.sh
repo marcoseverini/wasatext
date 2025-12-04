5c1ae7333bc35b1adabc580dc5f2d67c433a9daa
Marco Severini <marcoseverini5@gmail.com>
2025-12-04 17:06:55 +0100

    docker bug fix?

--- build_docker.sh
+++ build_docker.sh
@@ -3,12 +3,12 @@
 # Costruisci le immagini Docker
 if [ "$1" == "front" ]; then
     echo "Building frontend..."
-    docker build -t wasa-text-frontend:latest -f Dockerfile.frontend .
+    sudo docker build -t wasa-text-frontend:latest -f Dockerfile.frontend .
 elif [ "$1" == "back" ]; then
     echo "Building backend..."
-    docker build -t wasa-text-backend:latest -f Dockerfile.backend .
+    sudo docker build -t wasa-text-backend:latest -f Dockerfile.backend .
 else
     echo "Building frontend and backend..."
-    docker build -t wasa-text-frontend:latest -f Dockerfile.frontend .
-    docker build -t wasa-text-backend:latest -f Dockerfile.backend .
+    sudo docker build -t wasa-text-frontend:latest -f Dockerfile.frontend .
+    sudo docker build -t wasa-text-backend:latest -f Dockerfile.backend .
 fi
